fa2bad08dcf796ef19f581df89c41d4c7da643f8
ANKUSH <ankushchoudhary2019@gmail.com>
2026-01-12 09:15:39 -0800

    add login.es and als add get and poot route for login user

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express =  require("express");
 const router = express.Router();
 const User = require("../models/User.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-WrappAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 // Route to render the signup page
 
@@ -10,6 +10,9 @@ WrappAsync = require("../utils/wrapAsync.js");
     res.render("users/signup.ejs");
   });
 
+
+
+  // Routes set for store user POST-User Signup
   router.post(
     "/signup", 
     wrapAsync(async (req, res)=>{
@@ -26,5 +29,22 @@ WrappAsync = require("../utils/wrapAsync.js");
       }
     })
   );
+
+// Route to render the login page
+  router.get("/login", (req, res)=>{
+    res.render("users/login.ejs");
+  });
+  router.post(
+    "/login", passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),   
+    wrapAsync(async (req, res)=>{
+      req.flash("success", "Welcome back to wonderlust!");
+
+      res.redirect("/listings");
+    })
+  );
+  
 
 module.exports = router;
