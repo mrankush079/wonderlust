cac6e23d330142f9947a0ef6b1a675d018a10f85
ANKUSH <ankushchoudhary2019@gmail.com>
2026-01-13 01:15:12 -0800

    Add logout User method in our user.js

--- routes/user.js
+++ routes/user.js
@@ -45,6 +45,16 @@ const passport = require("passport");
       res.redirect("/listings");
     })
   );
-  
+
+
+  router.get("/logout", (req, res, next)=>{
+    req.logout((err)=>{
+      if(err){
+        return next(err);
+      }
+      req.flash("success", "You are logged out!");
+      res.redirect("/listings");
+    });
+  });
 
 module.exports = router;
