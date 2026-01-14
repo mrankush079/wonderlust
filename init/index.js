fe334a215e01c050be214d4feb2d29f05a343637
ANKUSH <ankushchoudhary2019@gmail.com>
2026-01-14 00:30:51 -0800

    Add Listing Owner for All Listing

--- init/index.js
+++ init/index.js
@@ -18,6 +18,12 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "69674856f7bfa7766e6003ac",
+  }));
+
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
